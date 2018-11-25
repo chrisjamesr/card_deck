83bfd8a7e0f91bb58e97a06aa71727aae6b4e78d
chrisjamesr <chrisjames.r@gmail.com>
2018-11-25 10:28:22 -0500

    remove shuffle methods

--- deck.js
+++ deck.js
@@ -8,7 +8,7 @@ class Card {
 
 function makeDeck(){
   const suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
-  const names = ["1","2","3","4","5","6","7","8","9","10","J","Q","K","A"];
+  const names = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
   return class Deck{  
     constructor(){
       this.stack = suits.reduce((acc, suit, i, arr)=>{
@@ -16,56 +16,15 @@ function makeDeck(){
         return acc.concat(names.map(name=> new Card(name, suit, n++))) 
       },[])
     }
-      
-    cut(x){
-     return (/*this.stack = */ 
-        !!x ? [...this.stack.slice(x), ...this.stack.slice(0,x)] 
-          : this.stack
-       )
+    shuffle(){
+      return compose(shuffleOneToOne, rejoin, reverseOne, trisect)(this.stack)
     }
 
-
-    }  
+    show(){
+      return this.stack.forEach(card=> console.log(`${card.name} of ${card.suit}`))
+    }
   }
 }
-  
+ 
 const Deck = new makeDeck()
 module.exports = new Deck();
-
-//     SUITS = ["Hearts", "Diamonds", "Clubs", "Spades"]
-//     NAMES = ["1","2","3","4","5","6","7","8","9","10","J","Q","K","A"]
-  
-//   def initialize()
-//     @deck = []
-
-//   end
-
-//   def make_deck
-//     SUITS.each do |suit|
-//       n = 0
-//       NAMES.each do |name|
-//         n+=1
-//         @deck.push( Card.new(suit: suit, name: name, value: n) )
-//       end
-//     end
-//     @deck
-//   end  
-
-//   def shuffle_deck
-//     show_deck.shuffle
-//   end
-// end  # End of Class
-  
-// class Game
-
-//   def initialize
-//     @deck = Deck.new
-//     @deck.make_deck
-//   end
-
-//   def shuffle_deck
-
-//   end
-
-// end
-
