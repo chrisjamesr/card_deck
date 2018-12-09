4223e9fec7f4e7b87f945f8fd2be392c48b47982
chrisjamesr <chrisjames.r@gmail.com>
2018-12-09 13:29:37 -0500

    replace shuffle methods with fisher-yates shuffle implementation

--- deck.js
+++ deck.js
@@ -9,105 +9,24 @@ class Card {
 function makeDeck() {
   const suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
   const names = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
-  const compose = (...fns) => (...args) => fns.reduceRight( (acc, fn) => fn(acc), ...args)
-  const applyMap = fn => {
-     return array => array.map(ele => fn(ele))
-  } 
-  const applyReduce = (fn) => {
-    return array => {
-      return array.reduce((acc, ele)=> fn(acc.concat(ele)))
-    }
-  }
-  const rejoin = (arr) => arr.reduce((acc,e)=>acc.concat(e))
-  
-  function bisect(arr){
-      return [arr.slice(0,arr.length/2), arr.slice(arr.length/2)]
-  }
-  // Accepts array of elements and shuffles one to one 
-  function shuffleSelection(arr){  
-    const shuffleOneToOne = (arr) => {
-        return arr.reduce((acc, ele, i, a) => {
-        acc[bisectAtIndex(i, a.length)] = ele
-        return acc
-        }, [])
-    }
-    const bisectAtIndex = (index, length = 52) => {
-        return index > (length - 1 - (index % 2)) / 2 ?
-        index - ((length-1+length%2)-index) : index + index
-    }
-    return shuffleOneToOne(arr)
-  }
-   
-  //accepts array of arrays and reverses even indexed arrays
- 
-  function reverseSelected(arr){
-    const reverseEvenIndexed = (arr) => {
-        return arr.map((e, i) => i % 2 === 0 ? e.reverse() : e)
-    }  
-    return reverseEvenIndexed(arr) 
-  }
-  
-  function shuffleEach(arr){
-      return applyMap(shuffleSelection)(arr)
-  }
-  
-  function combineAll(arr){
-      return rejoin(arr)
-  }
-  
-  function shuffleHalf(arr){
-      return compose(bisect)(arr)
-  }  
 
-  const smallDivisor = (divisorArr) => divisorArr.find((e, i, a)=> e * a[i-1] > a[i+1])   
-  const largeDivisor = (divisorArr) => divisorArr.find((e, i, a)=> e * a[i+1] > a[i+2])    
-
-  function divideSections(sectionQuantityFunction){
-    return function (arr){
-
-      const divisorArray = (arr) => {
-        return [...arr.keys()].filter((ele, i, a)=> {
-            return a.length % i === 0
-        })
-      } 
-      const sectionArray = (sectionQuantityFunction) => (array) => {
-        const sections = sectionQuantityFunction(array)      
-        return Array.from({length: sections}, (ele, index) => { 
-          return Array.from(
-            {
-              length: array.length/sections
-            }, (e,i) => array[index * array.length/sections + i]
-          )
-        })
-     }     
-    return sectionArray(compose(sectionQuantityFunction, divisorArray))(arr)
-   }
+  function shuffleDeck(stackArray) {
+    let m = stackArray.length;
+    let i, t;    
+    while(m) {
+        i = Math.floor(Math.random() * m--);        
+        t = stackArray[m];
+        stackArray[m] = array[i];
+        stackArray[i] = t
+    }
+    return stackArray
   }
-  
-  const smallSections = divideSections(largeDivisor)
-  const largeSections = divideSections(smallDivisor)
-  
+
   function cutDeck(arr){
     return x => {
       x = x || Math.ceil(arr.length/2);
       return [...arr.slice(x), ...arr.slice(0,x)]
     }
-  }
-     
-  function shuffleAll(arr){
-     return compose(
-        shuffleSelection, 
-        combineAll,
-        reverseSelected, 
-        shuffleEach,
-        largeSections,
-        shuffleSelection,
-        combineAll,
-        reverseSelected, 
-        shuffleEach, 
-        smallSections, 
-        shuffleSelection
-        )(arr)
   }
 
   return function Deck() {
@@ -117,17 +36,18 @@ function makeDeck() {
       },[])
      
     const shuffle = () => {
-      return this.stack = shuffleAll(this.stack)
+      this.stack = shuffleDeck(this.stack)
+      return this
     }
     
     const cut = (x) => {
-      return this.stack = cutDeck(this.stack)(x)  
+      this.stack = cutDeck(this.stack)(x)  
+      return this
     }
 
     const show = () => {
-      return this.stack
+      return this.stack.forEach(c=> console.log(`${c.name} of ${c.suit}`))
     }
-
     return {
         show, shuffle, cut
     }
@@ -135,3 +55,7 @@ function makeDeck() {
 }
 
 const Deck = new makeDeck()
+module.exports = new Deck
+
+
+
